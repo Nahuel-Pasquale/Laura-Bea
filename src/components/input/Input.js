eb6127bb9b5d2dc0bc3081f7c04070cb39e78f9b
Matias Godoy <matiasjuliangodoy@gmail.com>
2022-10-10 14:26:44 -0300

    added padding to textarea

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -3,12 +3,12 @@ import { useController } from "react-hook-form";
 import styled from "styled-components";
 
 const FormNameDiv = styled.div`
-width: ${props => props.width};
-display: flex;
-justify-content: center;
-flex-direction: column;
-align-items: flex-start;
-gap: 10px;
+  width: ${(props) => props.width};
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
   @media (max-width: 475px) {
     width: 100%;
   }
@@ -18,9 +18,8 @@ const FormNameInput = styled.input`
   width: 100%;
   height: 35px;
   border: 1px solid var(--dark);
-  font-family: 'Roboto';
+  font-family: "Roboto";
   padding-left: 5px;
-  
 `;
 
 const FormNameTextArea = styled.textarea`
@@ -28,19 +27,19 @@ const FormNameTextArea = styled.textarea`
   height: 120px;
   border: 1px solid var(--dark);
   resize: none;
-  font-family: 'Roboto';
-  padding-left: 5px;
+  font-family: "Roboto";
+  padding: 5px 5px;
 `;
 
 const FormNameLabel = styled.label`
-  font-family: 'Manrope-regular';
+  font-family: "Manrope-regular";
   font-size: 0.8rem;
 `;
 
 const ErrorMsg = styled.p`
   color: red;
   font-size: 0.8rem;
-  font-family: 'Manrope-regular';
+  font-family: "Manrope-regular";
 `;
 
 const Input = ({
